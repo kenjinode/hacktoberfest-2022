705d98a18df9d63e83098852563fdcf3949c9bf8
usrhans <116822779+usrhans@users.noreply.github.com>
2022-10-27 21:49:12 +0530

    Create sortArray.js

---
+++ javascript/sortArray.js
@@ -0,0 +1,11 @@
+let arr = [4, 32, 2, 5, 8];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] > arr[j]) {
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+}
